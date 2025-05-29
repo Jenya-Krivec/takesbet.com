1522f5ed14125048e9053b880c4d97e4930e98a1
Jenya <zhenya.krivec@gmail.com>
2025-05-29 19:04:26 +0300

    Добавлено поле Прізвище в таблицю користувачів та нового користувача

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->updateOrCreate(
+            ['email' => 'Zhenya.krivec@gmail.com'],
+            ['email' => 'Zhenya.krivec@gmail.com',
+             'name' => 'Jenya',
+             'surname' => 'Krivets',
+             'password' => '$2y$12$kzSR/5nmtcc8n7mIR4WNt.gYBxd52m/qVxwfZd1CReyBzlk5xeQx.',
+            ]
+        );
+    }
+}
